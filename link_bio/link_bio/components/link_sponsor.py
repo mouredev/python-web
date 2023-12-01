4188c2a5f3f43ce11d7ed6ff6b19ff3b9682028c
Brais Moure <mouredev@gmail.com>
2023-12-01 07:09:34 +0100

    Correcciones para mejorar el rendimiento

--- link_bio/link_bio/components/link_sponsor.py
+++ link_bio/link_bio/components/link_sponsor.py
@@ -6,11 +6,10 @@ def link_sponsor(imagen: str, url: str, alt: str) -> rx.Component:
     return rx.link(
         rx.image(
             src=imagen,
-            height="100%",
-            width="100%",
+            height=Size.VERY_BIG.value,
+            aspect_ratio="5 / 2",
             alt=alt
         ),
         href=url,
-        is_external=True,
-        height=Size.VERY_BIG.value
+        is_external=True
     )
