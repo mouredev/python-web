b771a5a5687d4e930c11262744af8613d13da073
Brais Moure <mouredev@gmail.com>
2024-01-25 21:50:26 +0100

    Construcción del frontend en remoto

---
+++ link_bio/local_build.sh
@@ -0,0 +1,9 @@
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -1,9 +1,11 @@
+cd link_bio
+python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://mouredev-web.up.railway.app reflex export --frontend-only
+API_URL=https://api.moure.dev reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
