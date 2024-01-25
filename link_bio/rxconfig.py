c702234111208ea9abbc218bd946f4836dfbe0bc
Brais Moure <mouredev@gmail.com>
2024-01-25 21:37:20 +0100

    CORS

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -1,5 +1,9 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="link_bio"
+    app_name="link_bio",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://moure.dev"
+    ]
 )
