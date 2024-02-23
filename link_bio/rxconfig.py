9b60986648c0a40bc87d9829758f06b75f09066a
Brais Moure <mouredev@gmail.com>
2024-02-23 15:06:37 +0100

    Prueba: State

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://api.moure.dev",
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://moure.dev"
