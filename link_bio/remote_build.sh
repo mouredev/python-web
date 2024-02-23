5e90ac39d8f39c5c877cf5e727925f0587311dad
Brais Moure <mouredev@gmail.com>
2024-02-23 14:53:48 +0100

    Se establece la API_URL como variable de entorno en el script de build

--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -5,7 +5,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://api.moure.dev reflex export --frontend-only
+export API_URL=https://api.moure.dev
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
