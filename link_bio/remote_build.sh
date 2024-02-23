8007755f373a5a30cc4158708b578a1173260d9c
Brais Moure <mouredev@gmail.com>
2024-02-23 15:02:04 +0100

    Prueba: api_url desde la Config

--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -5,7 +5,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-export API_URL=https://api.moure.dev
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
