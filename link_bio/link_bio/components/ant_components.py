5f34f2afdac02b54bebe28dcbedd0793f53220b2
Brais Moure <mouredev@gmail.com>
2024-11-04 10:53:45 +0100

    Botón flotante

--- link_bio/link_bio/components/ant_components.py
+++ link_bio/link_bio/components/ant_components.py
@@ -1,14 +1,14 @@
-# import reflex as rx
-# from link_bio.styles.colors import Color
+import reflex as rx
+from link_bio.styles.colors import Color
 
 
-# class FloatButton(rx.Component):
-#     library = "antd"
-#     tag = "FloatButton"
-#     icon: rx.Var[rx.el.Img]
-#     href: rx.Var[str]
-#     target = "_blank"
-#     badge = {"dot": True, "color": Color.PRIMARY.value}
+class FloatButton(rx.Component):
+    library = "antd"
+    tag = "FloatButton"
+    icon: rx.Var[rx.el.Img]
+    href: rx.Var[str]
+    target = "_blank"
+    badge = {"dot": True, "color": Color.PRIMARY.value}
 
 
-# float_button = FloatButton.create
+float_button = FloatButton.create
