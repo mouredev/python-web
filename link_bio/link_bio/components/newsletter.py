70cb8786ea09b82f15e3dc9260b5a8c7cd24e4c3
Brais Moure <mouredev@gmail.com>
2025-11-20 10:09:03 +0100

    Curso de Bash

--- link_bio/link_bio/components/newsletter.py
+++ link_bio/link_bio/components/newsletter.py
@@ -1,5 +1,5 @@
-from annotated_types import T
 import reflex as rx
+
 import link_bio.constants as const
 from link_bio.components.link_button import link_button
 from link_bio.styles.colors import TextColor
@@ -12,21 +12,21 @@ def newsletter() -> rx.Component:
             "mouredev.log",
             "La newsletter de la comunidad para mantenerse al día",
             "/icons/news.svg",
-            const.NEWSLETTER_URL
+            const.NEWSLETTER_URL,
         ),
         rx.html(
             "<iframe src='https://embeds.beehiiv.com/c9c3f7b7-7ed9-428a-a58f-cb53577fa352?slim=true' data-test-id='beehiiv-embed' title='Formulario de suscripción newsletter mouredev pro' width='100%' height='52' frameborder='0' scrolling='no' style='margin: 0; border-radius: 6px !important; background-color: transparent;'></iframe>",
-            width="100%"
+            width="100%",
         ),
         rx.hstack(
             rx.icon("mail-check"),
             rx.text(
-                "Más de 90.000 personas ya siguen mis novedades y consejos",
+                "Más de 100.000 personas ya siguen mis novedades y consejos",
                 color=TextColor.LIGHT.value,
-                size=Spacing.MEDIUM_SMALL.value
+                size=Spacing.MEDIUM_SMALL.value,
             ),
-            align="center"
+            align="center",
         ),
         spacing=Spacing.DEFAULT.value,
-        width="100%"
+        width="100%",
     )
--- link_bio/link_bio/views/courses_links.py
+++ link_bio/link_bio/views/courses_links.py
@@ -1,8 +1,9 @@
 import reflex as rx
+
 import link_bio.constants as const
 from link_bio.components.link_button import link_button
-from link_bio.components.title import title
 from link_bio.components.newsletter import newsletter
+from link_bio.components.title import title
 from link_bio.styles.styles import Color, Spacing
 
 
@@ -10,53 +11,52 @@ def courses_links() -> rx.Component:
     return rx.vstack(
         title("Cursos gratis"),
         link_button(
-            "Bash/Shell desde cero [en desarrollo]",
+            "Bash/Shell desde cero",
             "Curso de terminal, línea de comandos y scripting",
             "/icons/terminal.svg",
             const.BASH_COURSE_URL,
-            highlight_color=Color.YELLOW
         ),
         link_button(
             "Java y POO desde cero",
             "Curso de +8h, 75 lecciones y 150 ejercicios",
             "/icons/java.svg",
-            const.JAVA_COURSE_URL
+            const.JAVA_COURSE_URL,
         ),
         link_button(
             "Python desde cero",
             "Curso de +44h: Fundamentos, frontend, backend, testing...",
             "/icons/python.svg",
-            const.PYTHON_COURSE_URL
+            const.PYTHON_COURSE_URL,
         ),
         link_button(
             "JavaScript desde cero",
             "Curso de +14h, 120 lecciones y 220 ejercicios",
             "/icons/js.svg",
-            const.JS_COURSE_URL
+            const.JS_COURSE_URL,
         ),
         link_button(
             "Git y GitHub",
             "Curso de 5h para aprender Git y GitHub desde cero",
             "/icons/git.svg",
-            const.GIT_COURSE_URL
+            const.GIT_COURSE_URL,
         ),
         link_button(
             "SQL y Bases de Datos",
             "Curso de 7h desde cero para aprender los fundamentos de SQL",
             "/icons/sql.svg",
-            const.SQL_COURSE_URL
+            const.SQL_COURSE_URL,
         ),
         link_button(
             "Retos de programación",
             "Practica lógica con ejercicios y proyectos reales",
             "/icons/challenges.png",
-            const.CODE_CHALLENGES_URL
+            const.CODE_CHALLENGES_URL,
         ),
         link_button(
             "Un día, un lenguaje",
             "Primeros pasos en los 11 lenguajes de programación más usados",
             "/icons/code.svg",
-            const.LANGUAGES_COURSE_URL
+            const.LANGUAGES_COURSE_URL,
         ),
         title("Mucho más en"),
         link_button(
@@ -65,7 +65,7 @@ def courses_links() -> rx.Component:
             "/icons/pro.svg",
             const.PRO_URL,
             True,
-            Color.ORANGE.value
+            Color.ORANGE.value,
         ),
         link_button(
             "Guías de programación",
@@ -73,19 +73,19 @@ def courses_links() -> rx.Component:
             "/icons/book.svg",
             const.RESOURCES_URL,
             True,
-            Color.GREEN.value
+            Color.GREEN.value,
         ),
         link_button(
             "Discord",
             "El chat y los grupos de estudio de la comunidad",
             "/icons/discord.svg",
-            const.DISCORD_URL
+            const.DISCORD_URL,
         ),
         link_button(
             "YouTube",
             "Cursos y tutoriales sobre desarrollo de software",
             "/icons/youtube.svg",
-            const.YOUTUBE_URL
+            const.YOUTUBE_URL,
         ),
         newsletter(),
         width="100%",
